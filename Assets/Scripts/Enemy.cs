21a348d7543b41cb97adfc15e358a93ad298bf0a
Navdeep Hayer <navdeephayer@gmail.com>
2014-10-31 12:11:33 -0400

    updated Enemey & spawning Scripts
    Updated code to the enemy and spawning script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
 	public Transform target;
+	public Transform spawnOnePoint;
+	public Transform spawnTwoPoint;
+	public Transform spawnThreePoint;
+
 	public int moveSpeed;
 	public int rotationSpeed;
 	public int maxDistance;
+	public GameObject spawnPointOne;
+	public GameObject spawnPointTwo;
+	public GameObject spawnPointThree;
+
+	int randomSelection;
+
 
 	private Transform myTransform;
 
@@ -16,30 +26,62 @@ public class Enemy : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GameObject go = GameObject.FindGameObjectWithTag ("Player");
+		GameObject sp1 = GameObject.FindGameObjectWithTag ("spawnOne");
+		GameObject sp2 = GameObject.FindGameObjectWithTag ("spawnTwo");
+		GameObject sp3 = GameObject.FindGameObjectWithTag ("spawnThree");
 
 		target = go.transform;
 
+		spawnOnePoint = sp1.transform;
+		spawnTwoPoint = sp2.transform;
+		spawnThreePoint = sp3.transform;
+
 		maxDistance = 2;
+
+		randomSelection = Random.Range (0, 3);
+
+
 	
 	}
 	// Update is called once per frame
 	void Update () {
 
-		//Debug.DrawLine (target.position, myTransform.position,Color.red);
 
-		
-		//look at player
-		myTransform.rotation = Quaternion.Slerp (myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position),rotationSpeed *Time.deltaTime);
+		//look at player 
+		myTransform.rotation = Quaternion.Slerp (myTransform.rotation, Quaternion.LookRotation (target.position - myTransform.position), rotationSpeed * Time.deltaTime);
 
-		
-		
-		if (Vector3.Distance (target.position, myTransform.position) > maxDistance) {
-		
-			
-						//move towards player
-						myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
-				}
+		if (Vector3.Distance (target.position, myTransform.position) > 5)
+		{
+			PathFinding();
+		}
 
-	
+		if (Vector3.Distance (target.position, myTransform.position) > maxDistance) 
+		{			
+			//chasePlayer();
+		}
+	}
+	void PathFinding()
+	{
+		if(randomSelection == 0)
+		{
+			myTransform.position = Vector3.MoveTowards (myTransform.position, spawnOnePoint.position, 5 * Time.deltaTime); 
+		}
+		if(randomSelection == 1)
+		{
+			myTransform.position = Vector3.MoveTowards (myTransform.position, spawnTwoPoint.position, 5 * Time.deltaTime);
+		}
+		if(randomSelection == 2)
+		{
+			myTransform.position = Vector3.MoveTowards (myTransform.position, spawnThreePoint.position, 5 * Time.deltaTime);
+		}
+
+	}
+	void chasePlayer()
+	{
+		myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+	}
+	void NodeMovement()
+	{
+
 	}
 }
--- Assets/Scripts/Spawning.cs
+++ Assets/Scripts/Spawning.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections.Generic;
 
 
@@ -8,10 +8,18 @@ public class Spawning : MonoBehaviour {
 	public GameObject enemy2;
 	public GameObject enemy3;
 	public GameObject spawn;
+	public GameObject spawnPointOne;
+	public GameObject spawnPointTwo;
+	public GameObject spawnPointThree;
 	public List<GameObject> enemyList = new List<GameObject>();
-
+	public List<GameObject> startSpawn = new List<GameObject> ();
+	public GameObject temp;
 	public float timmer;
 	public int enemyCount;
+	public int speed;
+	public int randomSelection;
+
+
 
 
 	// Use this for initialization
@@ -19,19 +27,29 @@ public class Spawning : MonoBehaviour {
 		enemyList.Add(enemy);
 		enemyList.Add(enemy2);
 		enemyList.Add(enemy3);
-	
+		startSpawn.Add(spawnPointOne);
+		startSpawn.Add(spawnPointTwo);
+		startSpawn.Add(spawnPointThree);
+
+		speed = 10;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-     	if (enemyCount < 10) 
+     	if (enemyCount < 100) 
 		{
 			timmer += Time.deltaTime;
 			if (timmer > 2) 
 			{
-			 Instantiate (enemyList[(int)Random.Range(0,enemyList.Count)], new Vector3 (spawn.transform.position.x, spawn.transform.position.y, spawn.transform.position.z), Quaternion.identity);
-			 enemyCount += 1;
+			randomSelection = Random.Range(0,startSpawn.Count);
+			
+				temp = Instantiate(enemyList[(int)Random.Range(0,enemyList.Count)], new Vector3 (spawn.transform.position.x, spawn.transform.position.y, spawn.transform.position.z), Quaternion.identity) as GameObject;
+
+
+
+
+	 enemyCount += 1;
 			 timmer = 0;
 			}
 		}
