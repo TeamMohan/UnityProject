9e0e6e1ee48c85ddb72465d850adbdfbb8bbf702
Louis Smith <someonewholives@hotmail.com>
2014-11-07 11:01:31 -0500

    Added Assets
    Added Menu, Buttons, Models, Health Bar Graphics, and button scripts

---
+++ Assets/Scripts/MenuBtnControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBtnControl : MonoBehaviour {
+
+	public string scene;
+	public bool exit;
+	
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown(){
+		if (exit == true){
+			Application.Quit();
+			print("Closing Game");
+		}
+		else{
+			Application.LoadLevel(scene);
+		}
+	}
+}
