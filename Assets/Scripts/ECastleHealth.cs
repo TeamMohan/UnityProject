7c6c00a5acf99c177dccefabaa6d3207d645c5bb
Justin Caguiat <justin.caguiat@gmail.com>
2014-11-07 11:06:56 -0500

    added castle health for both sides with health bar graphic

---
+++ Assets/Scripts/ECastleHealth.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ECastleHealth : MonoBehaviour {
+	
+	public static float health = 100;
+	public float maxHealth = 100;
+	public GameObject player;
+	public float distance;
+	
+	public Texture hback,hframe,hfront;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position,GameObject.FindGameObjectWithTag("EnemyCastle").transform.position);
+
+	}
+	void OnGUI()
+	{
+		Rect rect = new Rect(Screen.width - 250,10,200,20);
+		Rect rect2 = new Rect(Screen.width - 250,10,200 * health / maxHealth,20);
+		
+		if(distance <15)
+		{
+			GUI.Label(rect,"EnemyHealth");
+			GUI.DrawTexture(rect,hback);
+			
+			GUI.DrawTexture(rect2,hfront);
+			GUI.DrawTexture(rect,hframe);
+		}
+	}
+	
+	void OnMouseDown()
+	{
+		if(distance < 10.5f)
+		{
+			health -= 10;
+		
+		}
+		
+	}
+}
